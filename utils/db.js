452e08509fb1934a39c2a4dc9337e4156812f58e
Chukwuma Somtochukwu <chukssomzzy19@gmail.com>
2024-03-09 17:32:45 +0000

    IMPLEMENTED: mongodb client

---
+++ utils/db.js
@@ -0,0 +1,30 @@
+#!/usr/bin/node
+
+import { MongoClient } from 'mongodb';
+
+class DBClient {
+  constructor() {
+    const host = process.env.DB_HOST || 'localhost';
+    const port = process.env.DB_PORT || 27017;
+    const database = process.env.DB_DATABASE || 'files_manager';
+
+    const uri = `mongodb://${host}:${port}/${database}`;
+    this.client = new MongoClient(uri, { useUnifiedTopology: true });
+    this.client.connect();
+  }
+
+  isAlive() {
+    return this.client.isConnected();
+  }
+
+  async nbUsers() {
+    return this.client.db().collection('user').countDocuments();
+  }
+
+  async nbFiles() {
+    return this.client.db().collection('files').countDocuments();
+  }
+}
+
+const dbClient = new DBClient();
+export default dbClient;
