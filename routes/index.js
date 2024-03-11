afbd26d7ed3083bc601fd2753c914a96df73d438
Chukwuma Somtochukwu <chukssomzzy19@gmail.com>
2024-03-11 18:19:03 +0000

    UPDATED: error defined

--- routes/index.js
+++ routes/index.js
@@ -3,14 +3,15 @@
 import AppController from '../controllers/AppController';
 import UsersController from '../controllers/UsersController';
 import AuthController from '../controllers/AuthController';
+import { authConnect, xAuthConnect } from '../middleware/authentication';
 
 function indexRoute(app) {
   app.get('/status', AppController.getStatus);
   app.get('/stats', AppController.getStats);
   app.post('/users', UsersController.postNew);
-  app.get('/users/me', UsersController.getMe);
-  app.get('/connect', AuthController.getConnect);
-  app.get('/disconnect', AuthController.getDisconnect);
+  app.get('/users/me', xAuthConnect, UsersController.getMe);
+  app.get('/connect', authConnect, AuthController.getConnect);
+  app.get('/disconnect', xAuthConnect, AuthController.getDisconnect);
 }
 
 export default indexRoute;
