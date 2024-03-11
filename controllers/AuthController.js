51cfb19b81bcb21130813603b02325ebc18074ac
Chukwuma Somtochukwu <chukssomzzy19@gmail.com>
2024-03-11 03:04:58 +0000

    IMPLEMENTED: Authenticate a user

---
+++ controllers/AuthController.js
@@ -0,0 +1,70 @@
+#!/usr/bin/node
+
+import { ObjectId } from 'mongodb';
+import { uuid } from 'uuidv4';
+import dbClient from '../utils/db';
+import redisClient from '../utils/redis';
+
+class AuthController {
+  /**
+   * Uses basic authentication to authenticate a user  and
+   * generate token with uuidv4
+   * @params {request} req express request object
+   * @params {response} res express response object
+   * @return {response} The response sent to user from server
+   */
+  static async getConnect(req, res) {
+    if (!req.get('Authorization')) {
+      return res.status(403).json({ error: 'forbidden' });
+    }
+
+    let authHeader = req.get('Authorization');
+    authHeader = authHeader.split(' ');
+    if (authHeader[0].toLowerCase() !== 'basic') {
+      return res.status(403).json({ error: 'Must use basic auth' });
+    }
+
+    const authStr = Buffer.from(authHeader[1], 'base64').toString('utf-8');
+    const email = authStr.split(':')[0];
+    const password = authStr.split(':')[1];
+    if (!email || !password) {
+      return res.status(403).json({ error: 'basic auth must contain password and email' });
+    }
+
+    const user = await dbClient.client.db().collection('users').findOne({ email });
+    if (!user) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    const token = uuid();
+    const tokenKey = `auth_${token}`;
+
+    await redisClient.set(tokenKey, user._id, 24 * 60 * 60);
+    return res.status(200).json({ token });
+  }
+
+  /**
+   * unauthenticate a user
+   * @params {request} req express request object
+   * @params {response} res express response object
+   * @return {response} express response object
+   */
+  static async getDisconnect(req, res) {
+    if (!req.get('X-Token')) {
+      return res.status(403).json({ error: 'forbidden' });
+    }
+
+    const token = req.get('X-Token');
+    const tokenKey = `auth_${token}`;
+    const userId = await redisClient.get(tokenKey);
+    const user = await dbClient.client.db().collection('users').findOne({ _id: ObjectId(userId) });
+
+    if (!user) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+    await redisClient.del(tokenKey);
+    return res.status(204).json({});
+  }
+}
+
+export default AuthController;
--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,9 +1,17 @@
 #!/usr/bin/node
 
+import { ObjectId } from 'mongodb';
 import sha1 from 'sha1';
 import dbClient from '../utils/db';
+import redisClient from '../utils/redis';
 
 class UsersController {
+  /**
+   * registers a new user
+   * @params {request} req express request object
+   * @params {response} res express response object
+   * @return {response} res express response object
+   */
   static async postNew(req, res) {
     if (!req.body || !req.body.email) {
       return res.status(400).json({ error: 'Missing email' });
@@ -27,6 +35,31 @@ class UsersController {
       console.error('Create User Error', err);
       return res.status(500).json({ error: 'Server error' });
     }
+  }
+
+  /**
+   * Get user information
+   * @params {request} req express request object
+   * @params {response} res express response object
+   * @returns {response} express response object
+   */
+  static async getMe(req, res) {
+    const token = req.get('X-Token');
+    if (!token) {
+      return res.status(403).json({ error: 'forbidden' });
+    }
+
+    const tokenKey = `auth_${token}`;
+    const userId = await redisClient.get(tokenKey);
+    if (!userId) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    const user = await dbClient.client.db().collection('users').findOne({ _id: ObjectId(userId) });
+    if (!user) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+    return res.status(200).json({ email: user.email, id: user._id });
   }
 }
 
