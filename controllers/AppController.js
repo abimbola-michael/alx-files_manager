67661b6bd6ea4897bafbec9a09b98e655480288d
Abimbola Michael <abimbolamichael100.com>
2024-03-13 20:32:29 +0100

    Fixed somthing

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -5,13 +5,19 @@ import dbClient from '../utils/db';
 
 class AppController {
   static getStatus(req, res) {
-    res.status(200).json({ redis: redisClient.isAlive(), db: dbClient.isAlive() });
+    // res.status(200).json({ redis: redisClient.isAlive(), db: dbClient.isAlive() });
+    if (redisClient.isAlive() && dbClient.isAlive()) {
+      res.json({ redis: true, db: true });
+      res.end();
+    }
   }
 
   static getStats(req, res) {
-    Promise.all([dbClient.nbUsers(), dbClient.nbFiles()]).then(([users, files]) => {
-      res.status(200).json({ users, files });
-    });
+    Promise.all([dbClient.nbUsers(), dbClient.nbFiles()]).then(
+      ([users, files]) => {
+        res.status(200).json({ users, files });
+      },
+    );
   }
 }
 
