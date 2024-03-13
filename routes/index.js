fd7f4107654b45c2484fbbc187263b67d4018a72
Abimbola Michael <abimbolamichael100.com>
2024-03-13 20:18:33 +0100

    Use express router instead

--- routes/index.js
+++ routes/index.js
@@ -1,23 +1,37 @@
 #!/usr/bin/node
 
+import { Router } from 'express';
 import AppController from '../controllers/AppController';
 import UsersController from '../controllers/UsersController';
 import AuthController from '../controllers/AuthController';
 import FilesController from '../controllers/FilesController';
 import { authConnect, xAuthConnect } from '../middleware/authentication';
 
-function indexRoute(app) {
-  app.get('/status', AppController.getStatus);
-  app.get('/stats', AppController.getStats);
-  app.post('/users', UsersController.postNew);
-  app.get('/users/me', xAuthConnect, UsersController.getMe);
-  app.get('/connect', authConnect, AuthController.getConnect);
-  app.get('/disconnect', xAuthConnect, AuthController.getDisconnect);
-  app.post('/files', xAuthConnect, FilesController.postUpload);
-  app.get('/files/:id', xAuthConnect, FilesController.getShow);
-  app.get('/files', xAuthConnect, FilesController.getIndex);
-  app.put('/files/:id/publish', xAuthConnect, FilesController.putPublish);
-  app.put('/files/:id/unPublish', xAuthConnect, FilesController.putUnPublish);
-}
+const router = Router();
+// function indexRoute(app) {
+//   app.get('/status', AppController.getStatus);
+//   app.get('/stats', AppController.getStats);
+//   app.post('/users', UsersController.postNew);
+//   app.get('/users/me', xAuthConnect, UsersController.getMe);
+//   app.get('/connect', authConnect, AuthController.getConnect);
+//   app.get('/disconnect', xAuthConnect, AuthController.getDisconnect);
+//   app.post('/files', xAuthConnect, FilesController.postUpload);
+//   app.get('/files/:id', xAuthConnect, FilesController.getShow);
+//   app.get('/files', xAuthConnect, FilesController.getIndex);
+//   app.put('/files/:id/publish', xAuthConnect, FilesController.putPublish);
+//   app.put('/files/:id/unPublish', xAuthConnect, FilesController.putUnPublish);
+// }
+router.get('/status', AppController.getStatus);
+router.get('/stats', AppController.getStats);
+router.post('/users', UsersController.postNew);
+router.get('/users/me', xAuthConnect, UsersController.getMe);
+router.get('/connect', authConnect, AuthController.getConnect);
+router.get('/disconnect', xAuthConnect, AuthController.getDisconnect);
+router.post('/files', xAuthConnect, FilesController.postUpload);
+router.get('/files/:id', xAuthConnect, FilesController.getShow);
+router.get('/files', xAuthConnect, FilesController.getIndex);
+router.put('/files/:id/publish', xAuthConnect, FilesController.putPublish);
+router.put('/files/:id/unPublish', xAuthConnect, FilesController.putUnPublish);
 
-export default indexRoute;
+// export default indexRoute;
+export default router;
--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 #!/usr/bin/node
 
 import express from 'express';
-import indexRoute from './routes';
+// import indexRoute from './routes';
+import router from './routes/index';
 
 const PORT = process.env.PORT || 5000;
 const app = express();
 
 app.use(express.json());
-indexRoute(app);
+// indexRoute(app);
+app.use('/', router);
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
